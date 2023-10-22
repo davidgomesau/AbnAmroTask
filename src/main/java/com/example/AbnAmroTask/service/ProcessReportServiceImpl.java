c1a961b455e2dc12e0901a03ca19f4a66a1b8068
David Catanho <david@catanho.com>
2023-10-23 01:29:24 +1100

    formatting

--- src/main/java/com/example/AbnAmroTask/service/ProcessReportServiceImpl.java
+++ src/main/java/com/example/AbnAmroTask/service/ProcessReportServiceImpl.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
-public class ProcessReportServiceImpl implements ProcessReportService{
+public class ProcessReportServiceImpl implements ProcessReportService {
 
     private final TransactionSummaryRepository transactionSummaryRepository;
 
